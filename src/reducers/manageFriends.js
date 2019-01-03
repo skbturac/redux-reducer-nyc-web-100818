9e931ee92a2bf2fe72c33f64e41539542628ace0
skbtrc <skbsturac@live.com>
2019-01-03 10:26:54 -0500

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,11 @@
 export function manageFriends(state, action){
+  switch (action.type) {
+    case 'ADD_FRIEND':
+      return { friends: [...state.friends, action.friend] }
+    case 'REMOVE_FRIEND':
+      const friends = state.friends.filter(f => f.id !== action.id)
+        return ({ friends: friends })
+    default:
+      return state;
+  }
 }
